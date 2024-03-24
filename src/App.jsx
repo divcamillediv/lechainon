0de4303fd6264b5d8c2670be9c3e5a0ab2055ec9
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2024-03-23 23:20:02 -0400

    not sure how to do chat..

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import Login from './pages/home/login';
 import MenuResidentResident from './pages/menuResident/menuResident';
+import ChatResident from './pages/menuResident/chatResident';
 import CalendarPageResident from './pages/menuResident/calendarResident';
 import InterventionPlanPageResident from './pages/menuResident/interventionplanResident';
 import Footer from './components/footer/footer';
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<MenuResidentResident />} />
         <Route path="/calendar" element={<CalendarPageResident />} />
+        <Route path="/chat" element={<ChatResident />} />
         <Route path="/intervention-plan" element={<InterventionPlanPageResident />} />
       </Routes>
       <Footer />
